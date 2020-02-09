bd9a3546e3cac2bc7974dde67fcb00399b63d982
Ioannis Smyrnios <smirnios.giannis@gmail.com>
2020-02-09 15:59:09 +0100

    Hash location strategy to be compatible with current hosting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AngularAthensComponent} from './angular-athens/angular-athens.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppRoutingModule { }
