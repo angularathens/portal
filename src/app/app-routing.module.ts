b7550ec4666d6ed4dc9f65c4511a31ed214c3abe
Ioannis Smyrnios <smirnios.giannis@gmail.com>
2020-02-09 15:02:24 +0100

    Basic page layout, default route to angular-athens, link to meetup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AngularAthensComponent} from './angular-athens/angular-athens.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  { path: 'angular-athens', component: AngularAthensComponent }
+  { path: 'angular-athens', component: AngularAthensComponent },
+  { path: '',
+    redirectTo: '/angular-athens',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularAthensComponent } from './angular-athens/angular-athens.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AngularAthensComponent
+    AngularAthensComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
